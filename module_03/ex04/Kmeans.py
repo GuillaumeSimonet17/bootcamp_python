8fd2e4f0d54621a1a32c21b9c7441466e241c529
Guillaume <gusimone@z3r6p6.42lyon.fr>
2023-12-04 15:26:07 +0100

    error

--- module_03/ex04/Kmeans.py
+++ module_03/ex04/Kmeans.py
@@ -1,14 +1,129 @@
-# 3 parameters: filepath, max_iter and ncentroid
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import pandas as pd
+"""
 
-# DATA : height, weight and bone_density
+Pour evaluer la qualité du clustering:
+WCSS ==> ∑i=1 k  ∑j=1 ni ∥xij − ci∥^2
 
-# population: (label?) Venus, Earth, Mars and Belt asteroids
+"""
 
-# (centroide?)
+def load_csv(path):
+    return np.genfromtxt(path, delimiter=',', skip_header=1)
+    
+class KmeansClustering: # todo: elbow methode
+    def __init__(self, max_iter=10, k=5):
+        self.k = k # number of centroids (cluster)
+        self.max_iter = max_iter # number of max iterations to update the centroids
+        self.centroids = [] # values of the centroids
+        self.dim = 3
+        
+        
+    def euclidean_method(self, data_points):
+        return np.sqrt(np.sum((self.centroids - data_points)**2, axis=1))
 
-# d = V (x2 - x1)^2 + (Y2 - Y1)^2 + (Z2 - Z1)^2
+    def wcss_calcul(self, X, clusters_centers, clusters_indexes):
+        wcss = 0
+        for i, cluster_indexes in enumerate(clusters_indexes):
+            wcss += np.sum((X[cluster_indexes] - clusters_centers[i])**2)
+        return wcss
+    
+    def normalize_data(self, X):
+        X_min = np.nanmin(X, axis=(0, 1))
+        X_max = np.nanmax(X, axis=(0, 1))
+        return (X - X_min) / (X_max - X_min)
 
-# stop iter till max_iter or iter-1 is same than current\
-# calcul somme des distances (inertia) de tous les points par rapport a son centroide
+    def fit(self, X):
+        normalized_X = self.normalize_data(X)
+        self.centroids = np.random.uniform(0, 1, size=(self.k, self.dim))
+        # self.centroids = np.random.uniform(np.nanmin(X, axis=(0, 1)), np.nanmax(X, axis=(0, 1)), size=(self.k, self.dim))
+        prev_wcss = float('inf')
+        
+        for x in range(self.max_iter):
+            clusters_ids = []
+            res = 0
+            for data_vector in normalized_X:
+                distance = self.euclidean_method(data_vector)
+                cluster_id = np.argmin(distance)
+                clusters_ids.append(cluster_id)
 
-#trouver le nombre optimal de centroide avec le elbow method (WCSS)
+            clusters_ids = np.array(clusters_ids)
+
+            clusters_indexes = []
+            for i in range(self.k):
+                clusters_indexes.append(np.argwhere(clusters_ids == i))
+
+            clusters_centers = []
+            for i, indice in enumerate(clusters_indexes):
+                if len(indice) == 0:
+                    clusters_centers.append(self.centroids[i])
+                else:
+                    clusters_centers.append(np.mean(normalized_X[indice], axis=0)[0])
+
+            wcss = self.wcss_calcul(normalized_X, clusters_centers, clusters_indexes)
+            if wcss is None or wcss <= 0 or prev_wcss == 0 or np.isnan(wcss):
+                print('Convergence atteinte ou NaN rencontré. Arrêt de l\'algorithme.')
+                return
+            percent_change = abs(prev_wcss - wcss) / prev_wcss * 100
+            print('=====================')
+            print('Percentage Change =', percent_change)
+
+            if wcss < 1:
+                print('Converged!')
+                break
+            else:
+                self.centroids = np.array(clusters_centers)
+                prev_wcss = wcss
+
+
+
+            #     #'''
+            # #DISPLAY:  
+            #     fig = plt.figure(figsize=(10, 6))
+                
+            # # -- 2D -- 
+            #     # fig, ax = plt.subplots(figsize=(10, 6))
+    
+            # # Affichage des points dans l'espace 3D
+            #     ax = fig.add_subplot(111, projection='3d')
+            #     ax.scatter(X[:, :, 0], X[:, :, 1], X[:, :, 2], c=clusters_ids)
+            #     ax.scatter(self.centroids[:, :, 0], self.centroids[:, :, 1], self.centroids[:, :, 2],
+            #                c=range(len(self.centroids)), marker='x', s=200)
+    
+            #     ax.set_xlabel('height')
+            #     ax.set_ylabel('weight')
+            #     ax.set_zlabel('bone_density')
+                
+                '''
+            # Change POV:
+                elevation_angle = 20  # Angle d'élévation
+                azimuth_angle = 30  # Angle azimutal
+                ax.view_init(elev=elevation_angle, azim=azimuth_angle)
+                '''
+                
+        # plt.show()
+                
+                # time.sleep(0.5)
+                #'''
+                
+            
+        return clusters_ids
+
+
+
+
+def main():
+    kmeans = KmeansClustering()
+    csv_values = load_csv("solar_system_census.csv")
+    X = np.array(csv_values)
+    selected_columns = X[:, 1:]
+
+    # Création de la matrice de vecteurs
+    result_matrix = np.reshape(selected_columns, (len(X), -1, 3))
+
+    kmeans.fit(result_matrix)
+    
+
+if __name__ == "__main__":
+    main()
